58eea80ec376056429356913449b95657ef4d4c0
shutanzzz <45780284+shutanzzz@users.noreply.github.com>
2023-05-27 21:37:08 +0800

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -103,7 +103,7 @@ def parse_args():
     parser.add_argument(
         "--num_warmup_steps", type=int, default=0, help="Number of steps for the warmup in the lr scheduler."
     )
-    parser.add_argument("--output_dir", type=str, default='../ckpt/', help="Where to store the final model.")
+    parser.add_argument("--output_dir", type=str, default='./ckpt/', help="Where to store the final model.")
     parser.add_argument("--seed", type=int, default=42, help="A seed for reproducible training.")
     parser.add_argument(
         "--max_argument_length",
